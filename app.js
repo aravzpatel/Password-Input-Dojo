ba6eb15414e009ccf700672c95f344af6eaeb8b6
Arav Salter Patel <arav.patel@oaknorth.co.uk>
2022-11-18 06:58:13 +0000

    Tidied up files :D

--- app.js
+++ app.js
@@ -11,21 +11,11 @@ var app = express();
 app.listen(8000);
 console.log("App is listening on port 8000")
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
 
-app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
 
 // error handler
 app.use(function (err, req, res, next) {
