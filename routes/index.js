46e80513861bbb7caf96181632a4d70f0ca90876
Naresh Ghanate <naresh.ghanate@oaknorth.co.uk>
2022-11-18 17:04:32 +0530

    Password Input Field Validation, Create an endpoint that checks whether a password field is valid.

--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,25 @@ var router = express.Router();
 
 router.get("/", (req, res, next) => res.status(200).send({ message: "ok" }));
 
+router.post("/validate-password", (req, res, next) => {
+    let errors = [];
+    const password = req.body.password;
+    if (password.length<8){
+        errors.push("Password must be at least 8 characters");
+    }
+    if(!/.*[0-9].*[0-9].*/.test(password)){
+        errors.push("Password must contain at least 2 numbers");
+    }
+    if(!/.*[A-Z].*/.test(password)){
+        errors.push("Password must contain at least one capital letter");
+    } 
+    if(!/.*[!@#$%^&].*/.test(password)){
+        errors.push("Password must contain at least one special character");
+    }
+    if(errors.length>0){
+        return res.status(200).send({ message: errors.join("\n") });    
+    }
+    return res.status(200).send({ message: "ok" });
+});
+
 module.exports = router;
--- routes/index.test.js
+++ routes/index.test.js
@@ -7,4 +7,36 @@ describe("Index file", () => {
     expect(res.statusCode).toBe(200);
     expect(res.body).toEqual({message: 'ok'})
   });
+
+  it("The password is not 8 characters long", async () => {
+    const res = await request(app).post("/validate-password").send({password: 'John$12'});
+    expect(res.statusCode).toBe(200);
+    expect(res.body.message).toEqual('Password must be at least 8 characters')
+  });
+
+  it("The password must contain at least 2 numbers", async () => {
+    const res = await request(app).post("/validate-password").send({password: 'Johnaarav!1'});
+    expect(res.statusCode).toBe(200);
+    expect(res.body.message).toEqual('Password must contain at least 2 numbers')
+  });
+
+  it("The password is not 8 characters long and does not contain at least 2 numbers", async () => {
+    const res = await request(app).post("/validate-password").send({password: 'John#1'});
+    expect(res.statusCode).toBe(200);
+    expect(res.body.message).toEqual('Password must be at least 8 characters\nPassword must contain at least 2 numbers')
+  });
+
+  it("The password must contain at least one capital letter", async () => {
+    const res = await request(app).post("/validate-password").send({password: 'john@12doe'});
+    expect(res.statusCode).toBe(200);
+    expect(res.body.message).toEqual('Password must contain at least one capital letter')
+  });
+  
+  it("The password must contain at least one special character", async () => {
+    const res = await request(app).post("/validate-password").send({password: 'John12doe'});
+    expect(res.statusCode).toBe(200);
+    expect(res.body.message).toEqual('Password must contain at least one special character')
+  });
+  
+
 });
